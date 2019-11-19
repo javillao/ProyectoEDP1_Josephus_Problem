b435470c2ac5237849a459482c71376937ce9ee7
Jordy Villao <javillao@espol.edu.ec>
2019-11-19 07:29:18 -0500

    Version alpha de interfaz

--- CirculoDeLaMuerte/src/circulodelamuerte/FXMLDocumentController.java
+++ CirculoDeLaMuerte/src/circulodelamuerte/FXMLDocumentController.java
@@ -10,26 +10,47 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 /**
  *
  * @author JordyVillao
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    @FXML private Slider sldr_NumberPeople;
+    @FXML private ChoiceBox chbx_InitialPosition;
+    @FXML private RadioButton rdbtn_Clockwise;
+    @FXML private RadioButton rdbtn_Anticlockwise;
+    @FXML private Button btn_Start;
+    @FXML private Pane pane_Container;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+    }   
+   
+    public void startSimulation(ActionEvent event){
+        int centerX = 120;
+        int centerY = 150; //Valores tomados con medicion manual en SceneBuilder
+        double r = 60.0;
+        double basisAngle = 360.0/5.0;
+        for(int i = 0; i < 5; i++ ){
+            double posX = r * Math.cos(Math.toRadians(basisAngle*i));
+            double posY = r * Math.sin(Math.toRadians(basisAngle*i));
+            Circle circle = new Circle(); 
+            circle.setCenterX(posX + centerY);
+            circle.setCenterY(posY + centerX);
+            circle.setRadius(10);
+            circle.setFill(Color.RED);
+            pane_Container.getChildren().add(circle);
+            System.out.println("Circulo creado");
+        } 
+    }
     
 }
