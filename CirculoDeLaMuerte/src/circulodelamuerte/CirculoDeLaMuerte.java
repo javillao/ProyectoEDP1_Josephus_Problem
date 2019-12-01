bb14a731a637ec18b197fb8cb2848b9c1efa1a43
Jordy Villao <javillao@espol.edu.ec>
2019-11-30 19:37:41 -0500

    Maybe, last commit

--- CirculoDeLaMuerte/src/circulodelamuerte/CirculoDeLaMuerte.java
+++ CirculoDeLaMuerte/src/circulodelamuerte/CirculoDeLaMuerte.java
@@ -23,6 +23,7 @@ public class CirculoDeLaMuerte extends Application {
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Círculo de la muerte de Josefo Flavio");
         stage.setScene(scene);
         stage.show();
     }
