264ff58e7ddbec5a525d31e32015819d64f830f5
Jordy Villao <jordy.villao@funiber.org>
2026-07-18 10:43:38 -0500

    Reorganize files into new folder structure

---
+++ CirculoDeLaMuerte/src/main/java/circulodelamuerte/Main.java
@@ -0,0 +1,7 @@
+package circulodelamuerte;
+
+public class Main {
+    public static void main(String[] args) {
+        CirculoDeLaMuerte.main(args);
+    }
+}
---
+++ CirculoDeLaMuerte/src/main/java/logic/Node.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author Grupo12
+ * @param <E>
+ */
+public class Node<E> {
+    private E content;
+    private Node<E> next;
+    private Node<E> prev;
+    
+    public Node(E data){
+        this.content = data;
+        this.prev = this.next = null;
+    }
+
+    public E getContent() {
+        return content;
+    }
+
+    public void setData(E data) {
+        this.content = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    } 
+    
+    public Node<E> getPrev(){
+        return prev;
+    }
+    
+    public void setPrev(Node<E> prev){
+        this.prev = prev;
+    }
+        
+}
